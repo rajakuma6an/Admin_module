12e05a7cb1da9caa39ace0b3a7c7f532f002bb46
Raja Kumaran <rajakumaran.doodleblue@gmail.com>
2023-05-24 18:24:54 +0530

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,11 +7,7 @@ const Navbar = () => {
   return (
     <nav className={"dark_bg"}>
       <div className=" py-3 d-flex align-items-center justify-content-between">
-        <Link to="/" className="navbar-brand">
-          Where in the world?
-        </Link>
-
-        <button className="btn btn-link">{<FaMoon size={20} />}</button>
+      NavBar
       </div>
     </nav>
   );
--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,11 +1,9 @@
 const initialState = {
-  countries: [],
+  userDetails: {},
 };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case "SET_COUNTRIES":
-      return { ...state, countries: action.payload };
     default:
       return state;
   }
